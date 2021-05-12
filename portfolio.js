e3f3683741c6ec49318da6a05235ff28d98686ea
Ashwini-Pandya@g <ashwinipandya5@gmail.com>
2021-05-12 15:05:52 +0530

    Added a toggle button for navigation-bar in media query for small screen

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,13 @@
+function togglenav() {
+  var nav = document.querySelector(".list");
+  console.log(nav.style.display);
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
+}
 function darkmode() {
   var dark = document.body;
   dark.classList.toggle("dark-mode");
-  var light = document.getElementById("dark-mode");
-  if (light.textContent === "Dark-mode🌙") {
-    var bright = (light.textContent = "Light-mode ☀️");
-    console.log(bright);
-  } else {
-    light.textContent = "Dark-mode🌙";
-  }
 }
